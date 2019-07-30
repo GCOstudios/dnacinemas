5b006ef9be9e8d55fab8ded85309178708041e4d
Jose Guerra <jose@gcostudios.com>
2019-07-30 22:08:54 +0100

    updated navigation markup

--- header.php
+++ header.php
@@ -45,7 +45,9 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'dnacinemas' ); ?></button>
+      <div class="menu-panel">
+        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><span class="menu-button-text"><?php esc_html_e( 'Menu', 'dnacinemas' ); ?></span></button>
+      </div>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
