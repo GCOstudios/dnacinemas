2be86c59976f20c4d786b1b4570c026f419ddf3d
Jose Guerra <jose@gcostudios.com>
2019-08-04 21:45:38 +0100

    updated markup

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,10 +10,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <?php dnacinemas_post_thumbnail(); ?>
+
+  <?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
   
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<header class="entry-header" style="background-image: url('<?php echo $backgroundImg[0]; ?>');">
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    <button class="scroll-down">Scroll Down</button>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
