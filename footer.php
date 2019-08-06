c62015359b58c836d824428fd2ab1cbde0c5f0a4
Jose Guerra <jose@gcostudios.com>
2019-08-06 07:18:53 +0100

    updated markup

--- footer.php
+++ footer.php
@@ -14,11 +14,12 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+    <button class="scroll-up">Scroll Up</button>
 		<div class="site-info">
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Developed by %2$s' ), 'dnacinemas', '<a href="https://jguerra.dev">JGuerra</a>' );
-				?>
+      <?php
+      /* translators: 1: Theme name, 2: Theme author. */
+      printf( esc_html__( 'Developed by %2$s' ), 'dnacinemas', '<a href="https://jguerra.dev">JGuerra</a>' );
+      ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
