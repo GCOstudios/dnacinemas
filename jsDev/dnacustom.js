e2e283b84a46146d61a80f1e7e8bfe00ee3129b4
Jose Guerra <jose@gcostudios.com>
2019-08-06 22:13:19 +0100

    updated script

--- jsDev/dnacustom.js
+++ jsDev/dnacustom.js
@@ -5,15 +5,26 @@ jQuery(function($) {
   var ENHANCEMENTS;
 
   // ================================
-  // Home
+  // Enhancements
   // ================================
 
     ENHANCEMENTS = {
 
     init: function() {
+      this.navigationClose(),
       this.scrollDown(),
-      this.scrollUp();
-      // this.animations();
+      this.scrollUp(),
+      this.animations();
+    },
+
+    // Navigation auto-close on click
+    // ==============================================
+    navigationClose: function() {
+      $(document).on('click', function(event) {
+        if (!$(event.target).closest('.main-navigation').length) {
+          $('.main-navigation').removeClass('toggled');
+        }
+      });
     },
 
     // Hero Scroll Down Arrow
@@ -34,15 +45,22 @@ jQuery(function($) {
             scrollTop: 0
         }, 1000);
       });
-    }
+    },
 
     // Animate on scroll library
     // ==============================================
-    // animations: function() {
-    //   AOS.init({
-    //     duration: 1000
-    //   });
-    // }
+    animations: function() {
+      
+      // Initiate the animations
+      // ==========================
+      AOS.init({
+        duration: 1000
+      });
+
+      // Target elements to animate
+      // ===============================
+      // $('.entry-title').attr('data-aos', 'fade-up');
+    }
 
   };
   
